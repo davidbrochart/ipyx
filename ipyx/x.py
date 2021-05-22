a35c4f3b317d06a79e07f12ad98f4526d48e04cd
David Brochart <david.brochart@gmail.com>
2021-05-22 20:51:21 +0200

    Implement unary and binary operators

--- ipyx/x.py
+++ ipyx/x.py
@@ -8,11 +8,27 @@
 TODO: Add module docstring
 """
 
-from typing import Any, List
+from typing import Any, List, Callable
 
 from ipywidgets import DOMWidget  # type: ignore
 from traitlets import Unicode  # type: ignore
 from ._frontend import module_name, module_version
+
+
+def make_x(v: Any) -> "X":
+    if type(v) is X:
+        return v
+    return X(v)
+
+
+def register(func: Callable):
+    def wrapper(self: "X", inputs: List["X"]) -> "X":
+        x = func(self, inputs)
+        x._compute()
+        self._outputs.append(x)
+        return x
+
+    return wrapper
 
 
 class X(DOMWidget):
@@ -51,22 +67,66 @@ class X(DOMWidget):
         if self._operation:
             exec(self._operation)
 
-    def _add(self, x0: "X", x1: "X") -> "X":
-        x = X(
-            inputs=[x0, x1], operation="self.v = self._inputs[0].v + self._inputs[1].v"
-        )
-        x._compute()
-        self._outputs.append(x)
-        return x
 
-    def __add__(self, other: Any) -> "X":
-        return self._add(self, make_x(other))
+def make_unary(name: str, sign: str = ""):
+    @register
+    def operation(self, inputs: List["X"]) -> "X":
+        if sign:
+            expression = f"{sign}self._inputs[0].v"
+        else:
+            expression = f"{name}(self._inputs[0].v)"
+        return X(inputs=inputs, operation=f"self.v = {expression}")
 
-    def __radd__(self, other: Any) -> "X":
-        return self._add(make_x(other), self)
+    def normal(self) -> "X":
+        return operation(self, [self])
+
+    setattr(X, f"__{name}__", normal)
 
 
-def make_x(v: Any) -> X:
-    if type(v) is X:
-        return v
-    return X(v)
+def make_binary(name: str, sign: str = ""):
+    @register
+    def operation(self, inputs: List["X"]) -> "X":
+        if sign:
+            expression = f"self._inputs[0].v {sign} self._inputs[1].v"
+        else:
+            expression = f"{name}(self._inputs[0].v, self._inputs[1].v)"
+        return X(inputs=inputs, operation=f"self.v = {expression}")
+
+    def normal(self, other: Any) -> "X":
+        return operation(self, [self, make_x(other)])
+
+    def reflected(self, other: Any) -> "X":
+        return operation(self, [make_x(other), self])
+
+    setattr(X, f"_{name}", operation)
+    setattr(X, f"__{name}__", normal)
+    setattr(X, f"__r{name}__", reflected)
+
+
+make_unary("neg", "-")
+make_unary("pos", "+")
+make_unary("abs")
+make_unary("invert", "~")
+make_unary("complex")
+make_unary("int")
+make_unary("float")
+# TODO: __index__ ?
+make_unary("round")
+make_unary("trunc")
+make_unary("floor")
+make_unary("ceil")
+
+make_binary("add", "+")
+make_binary("sub", "-")
+make_binary("mul", "*")
+make_binary("matmul", "@")
+make_binary("truediv", "/")
+make_binary("floordiv", "//")
+make_binary("mod", "%")
+make_binary("divmod")
+make_binary("pow", "**")
+make_binary("lshift", "<<")
+make_binary("rshift", ">>")
+make_binary("and", "&")
+make_binary("xor", "^")
+make_binary("or", "|")
