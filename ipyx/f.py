fa9a49689d9e3ffcbb125cfac1793b4808b7d299
David Brochart <david.brochart@gmail.com>
2021-09-12 09:04:20 +0200

    Support keyword arguments in function calls

--- ipyx/f.py
+++ ipyx/f.py
@@ -5,11 +5,19 @@ class F:
     def __init__(self, f):
         self.f = f
 
-    def __call__(self, *args):
+    def __call__(self, *args, **kwargs):
         inputs = [make_x(i) for i in args]
-        operation = "self.v = self._function(*[i.v for i in self._inputs])"
-        x = X(_inputs=inputs, _operation=operation, _function=self.f)
+        kwinputs = {k: make_x(i) for k, i in kwargs.items()}
+        operation = (
+            "self.v = self._function(*(i.v for i in self._inputs), "
+            "**{k: i.v for k, i in self._kwinputs.items()})"
+        )
+        x = X(
+            _inputs=inputs, _kwinputs=kwinputs, _operation=operation, _function=self.f
+        )
         x._compute()
         for i in inputs:
+            i._outputs.append(x)
+        for i in kwinputs.values():
             i._outputs.append(x)
         return x
